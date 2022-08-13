af7e57df18298feb83f57b5e6e416b038a01fee2
xwp <1520933757@qq.com>
2022-08-13 14:41:31 +0800

    Just DO It

---
+++ src/newTest.java
@@ -0,0 +1,13 @@
+public class newTest {
+    public static void main(String[] args) {
+        int add = Add(1, 6);
+        System.out.println(add);
+    }
+
+    public static int Add(int a, int b) {
+        return a + b;
+    }
+
+
+}
+
